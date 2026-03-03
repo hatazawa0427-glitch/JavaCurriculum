58c2375cab16dc0fd1d82bbb504997d44a2d6b60
hataz <hataz@kururuHP>
2026-03-03 16:44:18 +0900

    変数課題Q1~Q13

---
+++ HelloWorld_HatazawaMoe/src/curriculum/a/Question1.java
@@ -0,0 +1,95 @@
+package curriculum.a;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//Q1,Q2
+		byte byteNum = 10;
+		short shortNum = 100;
+		int intNum = 1000;
+		long longNum = 10000;
+		float floatNum = 9.5f;
+		double doubleNum = 10.5;
+		char charletter = 'a';
+		String stringletters = "ハロー";
+		boolean isBoolean = true;
+		
+		System.out.println(byteNum);
+		System.out.println(shortNum);
+		System.out.println(intNum);
+		System.out.println(longNum);
+		System.out.println(floatNum);
+		System.out.println(doubleNum);
+		System.out.println(charletter);
+		System.out.println(stringletters);
+		System.out.println(isBoolean);
+		
+	
+		//Q3
+		System.out.println(byteNum+shortNum+intNum+longNum);
+		System.out.println(byteNum*2);
+		System.out.println(charletter+stringletters+isBoolean);
+		System.out.println(byteNum+shortNum+intNum+longNum+floatNum+doubleNum);
+		System.out.println(byteNum*shortNum*intNum*longNum);
+		System.out.println(doubleNum/shortNum);
+		System.out.println(byteNum-shortNum);
+		
+		//Q4
+		String name = "山田太郎";
+		System.out.println("こんにちは、"+name+"さん！");
+		
+		//Q5
+		int age = 25;
+		System.out.println("年齢:"+age+"歳");
+		
+		//Q6
+		int num1 = 10;
+		int num2 = 5;
+		int sum = num1+num2;
+		System.out.println(sum);
+		
+		//Q7
+		int score = 80;
+		score=score+20;
+		System.out.println("最終スコア："+score);
+		
+		//Q8
+		double price=99.99;
+		int douPrice=(int)price;
+		System.out.println("整数価格："+douPrice);
+		
+		
+		//Q9
+		String numStr="123";
+		int str =Integer.parseInt(numStr);
+		System.out.println("返還後の値："+(str+10));
+		
+		//Q10
+		int num=50;
+		String numStr1=String.valueOf(num);
+		System.out.println("得点："+num+"点");
+		
+		//Q11
+		int a=10;
+		int b=20;
+		boolean result=a<b;
+		System.out.println(result);
+		
+		//Q12
+		int x=15;
+		String X=(x>=10) ? "OK":"NG";
+		System.out.println(X);
+		
+		//Q13
+		String text=("私はJavaが好きです。Javaは楽しい！");
+		String result2=text.replace("Java"," Python");
+		System.out.println(result2);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ HelloWorld_HatazawaMoe/src/sample/SelfIntroduction.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "畑澤 萌恵";
+		int age = 31;
+		String hobby = "映画鑑賞";
+		System.out.println("私の名前は"+name+"です。");
+		System.out.println("年齢は"+age+"歳です。");
+		System.out.println("趣味は"+hobby+"です。");
+		
+
+	}
+
+}
